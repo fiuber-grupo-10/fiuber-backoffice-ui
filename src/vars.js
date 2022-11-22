cd11c2315a95c4d042f675d59d5c3387f81aa856
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2022-11-21 22:23:45 -0300

    Added url backoffice to vars.js

--- src/vars.js
+++ src/vars.js
@@ -1,2 +1,3 @@
 export let url = "https://fiuber-api-gateway-nginx.herokuapp.com";
 export let urlUsers = "https://fiuber-api-usuarios-node-prod.herokuapp.com/"
+export let urlBackoffice = "https://fiuber-backoffice-node.herokuapp.com/"
