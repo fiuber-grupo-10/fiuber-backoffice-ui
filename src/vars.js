8fbaa805c7fa9787f479c551255679111788a4fb
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2022-12-09 16:34:24 -0300

    Changed heroku for okteto in vars.js

--- src/vars.js
+++ src/vars.js
@@ -1,3 +1,3 @@
 export let url = "https://fiuber-api-gateway-nginx.herokuapp.com";
-export let urlUsers = "https://fiuber-api-usuarios-node-prod.herokuapp.com/"
-export let urlBackoffice = "https://fiuber-backoffice-node.herokuapp.com/"
+export let urlUsers = "https://fiuber-api-usuarios-node-fiuber-dev-leogm99.cloud.okteto.net/"
+export let urlBackoffice = "https://fiuber-backoffice-node-fiuber-dev-leogm99.cloud.okteto.net/"
